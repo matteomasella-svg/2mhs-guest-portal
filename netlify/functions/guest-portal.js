9ccdc14374d4e8576d64d3e35826a8d4ed7ba867
Matteo <matteo.masella@duemmeconsulting.it>
2026-06-30 18:07:06 +0200

    Refactor requestUrl function and improve error handling

--- netlify/functions/guest-portal.js
+++ netlify/functions/guest-portal.js
@@ -3,40 +3,22 @@ const https = require("https");
 const APPS_SCRIPT_ENDPOINT =
   "https://script.google.com/macros/s/AKfycbzfelg-0p1xuHhSINGBPKHveYEtuJu6pruflAeElV0ezHZBrWDDM7622YdDbyc_i6Xh/exec";
 
-function requestUrl(url) {
+function requestUrl(url, redirects = 0) {
   return new Promise((resolve, reject) => {
-    https
-      .get(url, (res) => {
-        let data = "";
+    https.get(url, (res) => {
+      if ([301, 302, 303, 307, 308].includes(res.statusCode) && res.headers.location) {
+        if (redirects > 5) return reject(new Error("Too many redirects"));
+        return resolve(requestUrl(res.headers.location, redirects + 1));
+      }
 
-        res.on("data", (chunk) => {
-          data += chunk;
-        });
-
-        res.on("end", () => {
-          resolve({
-            statusCode: res.statusCode,
-            body: data,
-          });
-        });
-      })
-      .on("error", reject);
+      let data = "";
+      res.on("data", chunk => data += chunk);
+      res.on("end", () => resolve(data));
+    }).on("error", reject);
   });
 }
 
-exports.handler = async function (event) {
-  if (event.httpMethod === "OPTIONS") {
-    return {
-      statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type",
-      },
-      body: "",
-    };
-  }
-
+exports.handler = async function(event) {
   try {
     const params = event.queryStringParameters || {};
     const action = params.action || "getBooking";
@@ -44,33 +26,31 @@ exports.handler = async function (event) {
 
     const url =
       APPS_SCRIPT_ENDPOINT +
-      "?action=" +
-      encodeURIComponent(action) +
-      "&code=" +
-      encodeURIComponent(code);
+      "?action=" + encodeURIComponent(action) +
+      "&code=" + encodeURIComponent(code);
 
-    const result = await requestUrl(url);
+    const body = await requestUrl(url);
 
     return {
       statusCode: 200,
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Origin": "*"
       },
-      body: result.body,
+      body
     };
   } catch (err) {
     return {
       statusCode: 500,
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Origin": "*"
       },
       body: JSON.stringify({
         ok: false,
-        error: "Netlify function error",
-        details: err.message,
-      }),
+        error: "Proxy error",
+        details: err.message
+      })
     };
   }
 };
